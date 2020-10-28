31460b119f2547e80382dbc891c6d8767b4df33b
Aditya Darmawan <adityadarmawan@Adityas-MacBook-Pro.local>
2020-10-28 18:29:57 +0700

    ignored local config

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from app import config
 
 
 app = Flask(__name__)
+app.config.from_object(config)
+print(app.debug)
--- app/config.py
+++ app/config.py
@@ -1,1 +1,11 @@
+DEBUG = False
+ENV = 'production'
+PORT = 5000
+
 REDIS_PREFIX = 'loginhero'
+
+
+try:
+    from app.local_config import *
+except ImportError:
+    pass
