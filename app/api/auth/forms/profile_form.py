150ddb0630fe01533581003cb7d26a4cfb9c9823
Aditya Darmawan <adityadarmawan@Adityas-MacBook-Pro.local>
2020-10-30 17:09:43 +0700

    added update user profile

---
+++ app/api/auth/forms/profile_form.py
@@ -0,0 +1,103 @@
+import sqlalchemy as sa
+from flask import g
+from flask_babel import lazy_gettext as _
+from marshmallow import Schema, validates, validates_schema, ValidationError, EXCLUDE
+from marshmallow.fields import String
+from marshmallow.validate import Length, Regexp
+from app.utils.connect_db import connect_db
+from app.models.auth import User
+
+
+class ProfileForm(Schema):
+    username = String(required=True, allow_none=False,
+        validate=[
+            Length(min=1, error=_('Username is required')),
+            Regexp(r'^[a-zA-Z_\-0-9]+$', error=_('Username cannot contain space and can only contain a-z,_,0-9'))
+        ],
+        error_messages={
+            'required': _('Username is required'),
+            'null': _('Username is required')
+        }
+    )
+    password = String(required=False, allow_none=True,
+        validate=[
+            Length(min=1, error=_('Password is required'))
+        ],
+        error_messages={
+            'required': _('Password is required'),
+            'null': _('Password is required')
+        }
+    )
+    confirm_password = String(required=False, allow_none=True,
+        validate=[
+            Length(min=1, error=_('Confirm Password is required'))
+        ],
+        error_messages={
+            'required': _('Confirm Password is required'),
+            'null': _('Confirm Password is required')
+        }
+    )
+    name = String(required=True, allow_none=False,
+        validate=[
+            Length(min=1, error=_('Name is required'))
+        ],
+        error_messages={
+            'required': _('Name is required'),
+            'null': _('Name is required')
+        }
+    )
+    email = String(required=True, allow_none=False,
+        validate=[
+            Length(min=1, error=_('Email is required'))
+        ],
+        error_messages={
+            'required': _('Email is required'),
+            'null': _('Email is required')
+        }
+    )
+
+    class Meta:
+        unknown = EXCLUDE
+
+    @validates('username')
+    def validate_username(self, username):
+        user = self.context.get('user')
+        connect_db()
+        check = g.db.execute(
+            sa.select([User.c.id]).where(
+                sa.and_(
+                    User.c.username == username,
+                    User.c.id != user.id
+                )
+            )
+        ).scalar()
+
+        if check:
+            raise ValidationError(_('Username %(username)s already exists. Please input another username.', username=username))
+
+    @validates_schema(skip_on_field_errors=True)
+    def validate_confirm_password(self, data, **kwargs):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if password and not confirm_password:
+            raise ValidationError(_('Confirm password is required'), 'confirm_password')
+
+        if password and confirm_password and confirm_password != password:
+            raise ValidationError(_('Confirm password does not match'), 'confirm_password')
+    
+    @validates('email')
+    def validate_email(self, email):
+        user = self.context.get('user')
+        connect_db()
+        check = g.db.execute(
+            sa.select([User.c.id]).where(
+                sa.and_(
+                    User.c.email == email,
+                    User.c.id != user.id
+                )
+            )
+        ).scalar()
+
+        if check:
+            raise ValidationError(_('Email %(email)s already exists. Please input another email.', email=email))
--- app/api/auth/views/update_user_profile.py
+++ app/api/auth/views/update_user_profile.py
@@ -1,5 +1,48 @@
-from flask import request
+import sqlalchemy as sa
+import jwt
+from flask import request, g
+from app.utils.user import get_payload
+from app.utils.response import format_response
+from flask_babel import lazy_gettext as _
+from app.models.auth import User
+from app.utils.connect_db import connect_db
+from app.api.auth.forms.profile_form import ProfileForm
+from werkzeug.security import generate_password_hash
 
 
 def update_user_profile():
-    pass
+    try:
+        payload = get_payload()
+    except jwt.exceptions.PyJWTError:
+        # TODO: explain the error more obviously
+        return format_response(data=None, success=False, message=_('You are not authorized'), status_code=401)
+
+    user_id = payload['id']
+    connect_db()
+    user = g.db.execute(
+        sa.select([User]).where(User.c.id == user_id)
+    ).fetchone()
+
+    form = ProfileForm(context={'user': user}, partial=True)
+    data = form.load(request.json or {})
+
+    values_to_update = {
+        'modified': sa.func.NOW()
+    }
+    for key in data:
+        if key in ['username', 'name', 'email']:
+            values_to_update.update({
+                key: data[key]
+            })
+        elif key == 'password':
+            values_to_update.update({
+                'password': generate_password_hash(data[key])
+            })
+
+    transaction = g.db.begin()
+    g.db.execute(
+        User.update().values(**values_to_update).where(User.c.id == user_id)
+    )
+    transaction.commit()
+
+    return format_response(data=None)
